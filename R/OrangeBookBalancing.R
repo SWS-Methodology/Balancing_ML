a99d2fd45fd0173ff3d0b75dc45050fbd620232d
marcogarieri <marcogarieri@gmail.com>
2015-07-20 10:45:22 +0200

    Removing comments

--- R/OrangeBookBalancing.R
+++ R/OrangeBookBalancing.R
@@ -5,20 +5,7 @@ source("defaultStandardizationParametersBalancing.R")
 parameters = defaultStandardizationParametersBalancing()
 parameters.df = plyr::ldply(parameters$elements, data.frame,.id = "element")
 
-## Just for myself to understand
-#fbsElements = as.character(c(5113, 5025, 5312, 5510, 5327, 5421, 5520, 5525,
-#                5120, 5023, 5141, 71, 5600, 5900))
-#legend = data.frame(element = fbsElements,
-#                    names = c("OpeningStock", "AreaSown", "AreaHarv", "Production",
-#                              "Input", "Yield", "Feed", "Seed", "Waste", "Processed",
-#                              "Food","StockChange", "Imports", "Exports"))
-
-## Name of the elements
-#merge(example,legend,by="element")
-
 tableToBalance = merge(example,parameters.df,by="element")
-#tableToBalance[Value == 0 & standardDeviation == 0, LB := 0]
-#tableToBalance[Value == 0 & standardDeviation == 0, UB := 0]
 
 tableToBalance[,balancedValue := balancing(param1 = Value,
                                            param2 = standardDeviation,
